f115cd7303143e611603433f6b5a22af9024b878
Farid Mitri <faridmitri@gmail.com>
2020-12-15 00:30:47 +0200

    signUp activity

--- app/src/main/java/com/am/mathhero/SignUpActivity.java
+++ app/src/main/java/com/am/mathhero/SignUpActivity.java
@@ -1,14 +1,181 @@
 package com.am.mathhero;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.UUID;
 
 public class SignUpActivity extends AppCompatActivity {
+    EditText mail,password,username;
+    Button signUp;
+    ProgressBar progressBar;
+    ImageView imageView;
+    Uri imageUri;
+    boolean imageControl = false;
+
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mail = findViewById(R.id.editTextSignupMail);
+        password = findViewById(R.id.editTextSignupPassword);
+        signUp = findViewById(R.id.buttonSignupSign);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+        imageView = (ImageView) findViewById(R.id.imageViewCircle);
+        username = findViewById(R.id.editTextUsername);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp.setClickable(false);
+                String userEmail = mail.getText().toString();
+                String userPassword = password.getText().toString();
+                String userName = username.getText().toString();
+
+
+                if (!userEmail.equals("") && !userPassword.equals("") && !userName.equals(""))
+                {
+                    signUpFirebase(userEmail, userPassword,userName);
+                }
+                else
+                {
+                    Toast.makeText(SignUpActivity.this,
+                            "Complete all fields.",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+
+
+    public void signUpFirebase(String userEmail, String userPassword,String userName)
+    {
+        progressBar.setVisibility(View.VISIBLE);
+        auth.createUserWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful())
+                        {
+                            reference.child("Users").child(auth.getUid()).child("userName").setValue(userName);
+
+                            if(imageControl) {
+                                UUID randomID = UUID.randomUUID();
+                                final String imageName = "images/" + randomID + ".jpg";
+                                storageReference.child(imageName).putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                        StorageReference myStorageRef = firebaseStorage.getReference(imageName);
+                                        myStorageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                            @Override
+                                            public void onSuccess(Uri uri) {
+                                                String filePath = uri.toString();
+                                                reference.child("Users").child(auth.getUid()).child("image").setValue(filePath).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        Toast.makeText(SignUpActivity.this, "Write to database is successful.", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                }).addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Toast.makeText(SignUpActivity.this, "Write to database is not successful.", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    }
+                                });
+                                progressBar.setVisibility(View.INVISIBLE);
+                            }
+                             else
+                                {
+                                    reference.child("Users").child(auth.getUid()).child("image").setValue("null");
+                                }
+
+                                Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+
+                        else
+                        {
+                            Toast.makeText(SignUpActivity.this,
+                                    "There is a problem! Please try again later.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+        }
+
+
+    public void imageChooser()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,1);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null)
+        {
+            imageUri = data.getData();
+            Picasso.get().load(imageUri).into(imageView);
+            imageControl =  true;
+        }
+        else
+        {
+            imageControl = false;
+        }
     }
 }
