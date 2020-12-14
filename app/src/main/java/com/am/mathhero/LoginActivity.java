5d18f8884e674132d5d1c80c2c2064a735620808
Farid Mitri <faridmitri@gmail.com>
2020-12-15 00:39:20 +0200

    ligin activity

--- app/src/main/java/com/am/mathhero/LoginActivity.java
+++ app/src/main/java/com/am/mathhero/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.am.mathhero;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,10 +10,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -22,8 +28,21 @@ public class LoginActivity extends AppCompatActivity {
     SignInButton signinGoogle;
     TextView forgotPassword;
     ProgressBar progressBarSignin;
-   // FirebaseAuth auth = FirebaseAuth.getInstance();
     GoogleSignInClient googleSignInClient;
+    FirebaseAuth auth;
+    FirebaseUser firebaseUser;
+
+   /* @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null)
+        {
+            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
+    }*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +56,28 @@ public class LoginActivity extends AppCompatActivity {
         signUp = findViewById(R.id.buttonSignUp);
         forgotPassword = findViewById(R.id.textViewLoginForgotPassword);
         progressBarSignin = findViewById(R.id.progressBarSignin);
+        auth = FirebaseAuth.getInstance();
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Email =  mail.getText().toString();
+                String Password = password.getText().toString();
+
+                if (!Email.equals("") && !Password.equals(""))
+                {
+                    signin(Email,Password);
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Please enter an email and password.", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,5 +89,27 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void signin(String email,String password)
+    {
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful())
+                {
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(LoginActivity.this, "Sign in is successful.", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Sign in is not successful.", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
     }
 }
